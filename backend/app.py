8c001d2224af35a2af836dfefdf24dfb00189dc3
Andrii Butenko <andrejbutik@gmail.com>
2026-02-10 12:43:21 +0100

    added env

--- backend/app.py
+++ backend/app.py
@@ -36,6 +36,9 @@ def process_image():
     img_io.seek(0)
     
     return send_file(img_io, mimetype='image/png')
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    
+
+    app.run(debug=True, host='0.0.0.0', port=port)
